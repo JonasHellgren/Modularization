fb2dc5020b04cbdab4c511919085886d8843de55
Jonas Hellgren <jonas.hellgren@volvo.com>
2022-08-25 06:24:49 +0200

    try i LineExtractor

--- domain/src/main/java/domain/models/Line2D.java
+++ domain/src/main/java/domain/models/Line2D.java
@@ -2,10 +2,15 @@ package domain.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Line2D {
     private int x1,y1;
     private int x2,y2;
+
+   // private Line2D EMPTY_LINE=new Line2D();
+
 }
--- view_service/src/main/java/viewservice/logic/LineGenerator.java
+++ view_service/src/main/java/viewservice/logic/LineGenerator.java
@@ -3,47 +3,47 @@ package viewservice.logic;
 import domain.models.Dot2D;
 import domain.models.Edge3D;
 import domain.models.Line2D;
-import domain.models.Vertex3D;
 import lombok.extern.java.Log;
 import viewservice.view_mediator.MediatorMemberAbstract;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Log
 public class LineGenerator extends MediatorMemberAbstract {
 
 
     public List<Line2D>  getLines(List<Dot2D> dots) {
-
         List<Edge3D> edges=mediator.getEdges();
         List<Line2D> lines=new ArrayList<>();
-        long nofDots=dots.size();
-        System.out.println("nofDots = " + nofDots);
-        for (Edge3D e:edges) {
-
-            long dot1Idx=e.getVertexIdx1()-1;
-            long dot2Idx=e.getVertexIdx2()-1;
-
-            if (checkDotIdx(dot1Idx,dot1Idx,nofDots)) {
-                Dot2D dot1 = dots.get((int) dot1Idx);
-                Dot2D dot2 = dots.get((int) dot2Idx);
-                Line2D line=new Line2D(dot1.getX(), dot1.getY(), dot2.getX(), dot2.getY());
-                lines.add(line);
-
-            } else
-            {
-                log.warning("Fault dot index for line generation");
-            }
-
+        for (Edge3D edge:edges) {
+            Optional<Line2D> line= getLine(dots, edge);
+            line.ifPresent(lines::add);
         }
 
         return lines;
     }
 
-    private  boolean checkDotIdx(long dot1Idx, long dot2Idx, long nofDots)  {
-        return (dot1Idx>0 && dot1Idx<nofDots && dot2Idx>0 && dot2Idx<nofDots);
-    }
+    private Optional<Line2D> getLine(List<Dot2D> dots, Edge3D edge) {
+        long dot1Idx= edge.getVertexIdx1()-1;
+        long dot2Idx= edge.getVertexIdx2()-1;
+        Optional<Line2D> line=Optional.empty();
 
+        try {
+            Dot2D dot1 = dots.get((int) dot1Idx);
+            Dot2D dot2 = dots.get((int) dot2Idx);
+            line=Optional.of(new Line2D(dot1.getX(), dot1.getY(), dot2.getX(), dot2.getY()));
+
+
+        } catch (IndexOutOfBoundsException e)
+        {
+            log.warning("Fault dot index for line generation");
+        } catch (Exception e) {
+            log.warning("Unknown exception, class = "+e.getClass());
+        }
+
+        return line;
+    }
 
 }
